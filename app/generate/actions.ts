3f8e62eb919b7d2ec5d3229a92a36ecf9a3188f3
TariqKichawele <tariqkichawele66@gmail.com>
2024-11-04 15:53:11 -0500

    Implemented GetVideoLengthAndSubtitles using RapidAPI key

--- app/generate/actions.ts
+++ app/generate/actions.ts
@@ -1,1 +1,39 @@
-export const CreatePowerpoint = async (videoId: string) => {}
+'use server';
+
+import axios from 'axios';
+
+type VideoMetaData = {
+    length: number | null;
+    subtitlesURL: string | null;
+}
+
+export const CreatePowerpoint = async (videoId: string) => {
+
+}
+
+export async function GetVideoLengthAndSubtitles(videoId: string): Promise<VideoMetaData> {
+    try {
+        const options = {
+            method: 'GET',
+            url: 'https://yt-api.p.rapidapi.com/video/info',
+            params: {
+                id: videoId
+            },
+            headers: {
+                "x-rapidapi-key": process.env.RAPID_API_KEY,
+                "x-rapidapi-host": "yt-api.p.rapidapi.com"
+            }
+        } as const;
+
+        const res = await axios.request(options);
+
+        return {
+            length: res.data.lengthSeconds,
+            subtitlesURL: res.data.subtitles.subtitles.find(
+                (subtitle: { languageCode: string }) => subtitle.languageCode === 'en')?.url || null
+        };
+    } catch (error) {
+        console.error(error);
+        throw new Error('Failed to get video length and subtitles');
+    }
+}
